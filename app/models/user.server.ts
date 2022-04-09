884fa69fb7b785da63e1e44a24b524e45c67da6a
Drew Lytle <aml3ed@virginia.edu>
2022-04-09 08:10:33 -0400

    Prettier user.server

--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -1,8 +1,6 @@
 import type { Password, User } from "@prisma/client";
 import bcrypt from "@node-rs/bcrypt";
-
 import { prisma } from "~/db.server";
-
 export type { User } from "@prisma/client";
 
 export async function getUserById(id: User["id"]) {
